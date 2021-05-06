53f64e8f5055c60fc1fabb70d4c1010b71ad3f2c
Diggory Rycroft <diggory@segment.com>
2021-05-06 18:32:52 -0400

    Create my-function.js

---
+++ my-function.js
@@ -0,0 +1,16 @@
+[
+	{
+		destinations: ['blockEvent'],
+		eventType: ['communication'],
+		instructions: [
+			{
+				valueB: 'userIdError',
+				valueA: 'packet.event.payload.data.placement',
+				equal: 'true',
+				notEqual: 'false',
+				type: 'IfThen'
+			}
+		],
+		name: 'Kill Switch- userIdError',
+		component: ['LOGON']
+	}]
