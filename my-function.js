0d6e5d017f35f83c9fa80748d2f7d4ddb28fdaca
Diggory Rycroft <diggory@segment.com>
2021-05-06 20:42:54 -0400

    Update my-function.js

--- my-function.js
+++ my-function.js
@@ -1,17 +1,1 @@
-[
-	{
-		destinations: ['blockEvent'],
-		eventType: ['communication'],
-		testVar: ['successful'],
-		instructions: [
-			{
-				valueB: 'userIdError',
-				valueA: 'packet.event.payload.data.placement',
-				equal: 'true',
-				notEqual: 'false',
-				type: 'IfThen'
-			}
-		],
-		name: 'Kill Switch- userIdError',
-		component: ['LOGON']
-	}];
+{"foo":"bar"}
